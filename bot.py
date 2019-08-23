e6e3eb9ca5927bab1659616755b9162f83616a02
epselont <newrol35@gmail.com>
2019-08-23 16:04:56 +0300

    send_sticker

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import bot_config
 import telebot
+from pprint import pprint
 from telebot import apihelper
 
 TOKEN = bot_config.TOKEN
@@ -10,5 +11,14 @@ apihelper.proxy = {'https':'https://51.79.31.27:8080'}
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "Привет")
+
+@bot.message_handler(content_types=['sticker'])
+def print_message(message):
+    pprint(message.json)
+    bot.send_sticker(message.chat.id, "CAADAgADeQMAArarPwullRa4zzq2ThYE")
 
 bot.polling()
