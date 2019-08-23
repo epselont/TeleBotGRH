f197a673e3f07fc8a79070da21db6ba0c1b16263
epselont <newrol35@gmail.com>
2019-08-23 13:19:11 +0300

    init

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import bot_config
 import telebot
+from telebot import apihelper
 
 TOKEN = bot_config.TOKEN
 bot = telebot.TeleBot(TOKEN)
+
+apihelper.proxy = {'https':'https://51.79.31.27:8080'}
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
